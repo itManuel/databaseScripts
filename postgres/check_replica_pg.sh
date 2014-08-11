204d5603975e3e43e0b3811648aecf5ab4de2aef
Manuel Aller <manuel.aller@gmail.com>
2014-08-11 16:57:47 -0300

    script para chequear el lag de una réplica

---
+++ postgres/check_replica_pg.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# guarda 2 fechas, una por current_timestamp (funcion de postgres) y la otra con la fecha 'clavada'
+# compara las fechas y tira la cantidad de segundos de diferencia
+
+# armo el sql de update:
+if [ $0 -eq 'M' ]; then
+DATE=`/bin/date +'%Y-%m-%d %H:%M:%S'`
+
+echo "update replica_times set fecha_text='${DATE}';" |psql postgres
+
+else
+LOGFILE=/tmp/check_replica_$$
+# armo el sql del slave:
+echo "select date_part('seconds',age(current_timestamp,fecha_text))+60*date_part('minute',age(current_timestamp,fecha_text))+3600*date_part('hour',age(current_timestamp,fecha_text))+86400*date_part('days',age(current_timestamp,fecha_text)) as seconds from replica_times ;" |psql postgres |head -3 |tail -1 > $LOGFILE
+
+sort $LOGFILE|bc
+rm $LOGFILE
+
+fi
