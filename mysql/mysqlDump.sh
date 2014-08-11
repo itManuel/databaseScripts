320579f5fa465b6387aa273aceedeceb367ccbf9
Manuel Aller <manuel.aller@gmail.com>
2014-08-11 13:03:16 -0300

    some aestetic changes

--- mysql/mysqlDump.sh
+++ mysql/mysqlDump.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # script para full backup de mysql y guardar sólo N dias hacia atras
 
-DB_HOST=capstan-master-db.cg1e3xqngvlk.us-east-1.rds.amazonaws.com
-BACKUP_DIR=/opt/capstan/backups
+DB_HOST=yourDBhost
+BACKUP_DIR=/path/to/db_backups
 ARCHIVE_DAYS=60
 
 # Backup the DB
@@ -10,7 +10,7 @@ ARCHIVE_DAYS=60
 /usr/bin/mysqldump -u root --all-databases --routines | gzip > $BACKUP_DIR/$(/bin/hostname)-$(date +%Y-%m-%d).mysqldump.gz
 EXITVALUE=$?
 # Eliminate old backup files
-find $BACKUP_DIR -name "capstan*" -mtime +$ARCHIVE_DAYS -exec rm {} \; >/dev/null 2>&1
+find $BACKUP_DIR -name "*mysqldump.gz" -mtime +$ARCHIVE_DAYS -exec rm {} \; >/dev/null 2>&1
 
 exit 0
 
