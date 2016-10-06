314626dc7ad75b0e4754e7432bb925436588ce21
Manuel Aller <manuel.aller@globant.com>
2016-10-06 09:13:11 -0300

    backup script. very simple.

---
+++ postgres/do_backup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# simple database script for backup a single database
+#
+DB_NAME=desptaxis
+HOST_NAME=`uname -n`
+BACKUP_DIR=/DATA/backups
+ARCHIVE_DAYS=20
+
+/usr/bin/logger -t postgres-backup "backup started"
+
+/usr/bin/pg_dump -c -b $DB_NAME |gzip > ${BACKUP_DIR}/${HOST_NAME}-${DB_NAME}-$(date +%Y-%m-%d).psql.gz
+EXITVALUE=$?
+if [ $EXITVALUE != 0 ]; then
+	/usr/bin/logger -t postgres-backup-alert "ALERT exited abnormally with [$EXITVALUE]"
+fi
+/usr/bin/logger -t postgres-backup "backup ended"
+
+find $BACKUP_DIR -name "*${DB_NAME}*" -mtime +$ARCHIVE_DAYS -exec rm {} \; > /dev/null 2>&1
+
